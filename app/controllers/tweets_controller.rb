05d9ec3a35bab60b9149c8cd26728bb58b766dd0
Yurii Sushkov <s.supremus@gmail.com>
2021-01-09 13:50:31 +0200

    WIP

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+# tweets controller
+class TweetsController < ApplicationController
+  before_action :set_tweet, only: %i[show edit update destroy]
+
+  # GET /tweets
+  # GET /tweets.json
+  def index
+    @tweet = Tweet.new
+    @tweets = Tweet.order(created_at: :desc)
+  end
+
+  # GET /tweets/1
+  # GET /tweets/1.json
+  def show
+  end
+
+  # GET /tweets/new
+  def new
+    @tweet = Tweet.new
+  end
+
+  # GET /tweets/1/edit
+  def edit
+  end
+
+  # POST /tweets
+  # POST /tweets.json
+  def create
+    @tweet = Tweet.new(tweet_params)
+
+    respond_to do |format|
+      if @tweet.save
+        format.html { redirect_to tweets_path, notice: 'Tweet was successfully created.' }
+        format.json { render :index, status: :created }
+      else
+        format.html { render :new }
+        format.json { render json: @tweet.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /tweets/1
+  # PATCH/PUT /tweets/1.json
+  def update
+    respond_to do |format|
+      if @tweet.update(tweet_params)
+        format.html { redirect_to @tweet, notice: 'Tweet was successfully updated.' }
+        format.json { render :show, status: :ok, location: @tweet }
+      else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace(@tweet, partial: 'tweets/form', locals: { tweet: @tweet }) }
+        format.html { render :edit }
+        format.json { render json: @tweet.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /tweets/1
+  # DELETE /tweets/1.json
+  def destroy
+    @tweet.destroy
+    respond_to do |format|
+      format.html { redirect_to tweets_url, notice: 'Tweet was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_tweet
+      @tweet = Tweet.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def tweet_params
+      params.require(:tweet).permit(:body, :likes_count, :retweets_count)
+    end
+end
---
+++ app/models/tweet.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Tweet < ApplicationRecord
+  after_create_commit { broadcast_prepend_to 'tweets' }
+  after_update_commit { broadcast_replace_to 'tweets' }
+  after_destroy_commit { broadcast_remove_to 'tweets' }
+
+  validates :body, presence: true
+end
