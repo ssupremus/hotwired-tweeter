9e58e1f1c38be3d090b9faacfd62f76b44d2c19d
Yurii Sushkov <s.supremus@gmail.com>
2021-01-21 22:52:30 +0200

    Expand show link to the whole tweet (index only).

--- app/assets/javascripts/controllers/link_controller.js
+++ app/assets/javascripts/controllers/link_controller.js
@@ -1,9 +1,29 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  goTo() {
-    const url = this.element.href
+  static values = {
+    url: String
+  }
+  static targets = ["ignore"]
 
-    window.location = url
+  goTo() {
+    const url = this.urlValue
+
+    if (this.urlValue != window.location.pathname && !this.ignoreArea) {
+      window.location.pathname = url
+    }
+  }
+
+  get ignoreArea() {
+    const ignore  = this.ignoreTarget
+    let   elem    = event.target
+
+    while (elem.parentNode) {
+      elem = elem.parentNode
+      if (elem == ignore) {
+        return true
+      }
+    }
+    return false
   }
 }
