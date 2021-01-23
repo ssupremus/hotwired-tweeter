70356ff423e80ccccf5ed7f18e7a39d9876fafec
Yurii Sushkov <s.supremus@gmail.com>
2021-01-23 19:50:13 +0200

    Configure links to user inside the tweet turbo frame.

--- app/assets/javascripts/controllers/tweet_controller.js
+++ app/assets/javascripts/controllers/tweet_controller.js
@@ -4,7 +4,7 @@ export default class extends Controller {
   static values = {
     url: String
   }
-  static targets = ["ignore"]
+  static targets = ["ignore", "username", "avatar"]
 
   goTo() {
     const url = this.urlValue
@@ -16,10 +16,12 @@ export default class extends Controller {
 
   get ignoreArea() {
     const ignore    = this.ignoreTarget
+    const username  = this.usernameTarget
+    const avatar    = this.avatarTarget
     let   elem      = event.target
 
     while (elem.parentNode) {
-      if (elem == ignore) {
+      if (elem == ignore || elem == username || elem == avatar) {
         return true
       }
       elem = elem.parentNode
